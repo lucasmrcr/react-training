431be1e24070951b0ceebcdac8ba0b898817466d
lucasmercier <114911557+lucasmercierredlab@users.noreply.github.com>
2023-01-23 10:10:08 +0100

    feat(resume): add profile pic using picture component

--- src/components/resume/PersonalInformation.tsx
+++ src/components/resume/PersonalInformation.tsx
@@ -1,11 +1,15 @@
 import {FC} from 'react';
 import styled from 'styled-components';
+import Picture from '../shared/Picture';
+import profilePic from  '../../assets/images/profile_pic.jpg'
 
 const PersonalInformationContainer = styled.div`
   height: 100vh;
   background-color: yellow;
-`
+`;
 
-const PersonalInformation: FC = () => <PersonalInformationContainer ></PersonalInformationContainer>
+const PersonalInformation: FC = () => <PersonalInformationContainer>
+  <Picture src={profilePic} />
+</PersonalInformationContainer>;
 
 export default PersonalInformation;
