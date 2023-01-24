b4467297cde0bcf2f41065372a8b7345ca61fe37
lucasmercier <114911557+lucasmercierredlab@users.noreply.github.com>
2023-01-24 15:40:41 +0100

    feat(admin): add admin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,29 @@
-import React, {FC} from 'react';
-import Resume from './pages/resume/Resume';
+import React, {FC, lazy, Suspense} from 'react';
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
-const App: FC = () => <Resume />;
+
+const Resume = lazy(() => import('./pages/resume/Resume'));
+const Admin = lazy(() => import('./pages/admin/Admin'));
+
+const App: FC = () => <>
+  <Router>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">CV</Nav.Link>
+            <Nav.Link href="/admin">Admin</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Suspense fallback={<div>Chargement...</div>}>
+      <Routes>
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/" element={<Resume/>}/>
+      </Routes>
+    </Suspense>
+  </Router>
+</>;
 export default App;
